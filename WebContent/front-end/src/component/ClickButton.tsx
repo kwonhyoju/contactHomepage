d9d0c7806a5a7293a5683ea669e245deabcc596b
jhyooooon <jhyooooooon@gmail.com>
2021-01-04 18:34:07 +0900

    Remove remark

--- WebContent/front-end/src/component/ClickButton.tsx
+++ WebContent/front-end/src/component/ClickButton.tsx
@@ -3,29 +3,35 @@ import {inject,observer} from "mobx-react";
 import axios from "axios";
 import { IContactStore } from "../stores/IContactStore";
 
-type Props ={
-    contactStore?:IContactStore;
+interface Props {
+    contactStore?: IContactStore;
 }
 
 @inject("contactStore")
 @observer
-class ClickButton extends Component<Props, {}>{
+class ClickButton extends Component<Props>{
+    constructor(props: Props){
+        super(props);
+    }
+
     apiTestBtn=()=>{
         //TODO 서버를 연결 시키고 나서 데이터를 받아오지 못할때 contactStore뒤에 ? 가 영향이 있는지 확인학기
         //contactStore뒤에 ? 가 무슨 동작을 하는건지 찾기
-        this.props.contactStore?.ApiTest();
+        if(this.props.contactStore){
+            this.props.contactStore.ApiTest();
+        }
     } 
 
-    // testApi = async()=>{
-    //     try{
-    //         const apiData = await axios.get('http://localhost:8081/springMVCXml/');
-    //         return console.log(apiData);
-    //     }catch(error){  
-    //         console.log(":::error::::",error);
-    //     }
-    // }
+    testApi = async()=>{
+        try{
+            const apiData = await axios.get('http://localhost:8081/springMVCXml/');
+            return console.log(apiData);
+        }catch(error){  
+            console.log(":::error::::",error);
+        }
+    }
     componentDidMount(){
-        // this.testApi();
+        this.testApi();
     }
 
     
@@ -34,7 +40,7 @@ class ClickButton extends Component<Props, {}>{
 
         return(
             <div>
-                {/* <button onClick={this.apiTestBtn}>API button</button> */}
+                <button onClick={this.apiTestBtn}>API button</button>
             </div>
         );
     }
